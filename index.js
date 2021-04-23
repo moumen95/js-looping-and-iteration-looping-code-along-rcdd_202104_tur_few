339155b13ac7ea1933112f5a956dc23dd7aae658
Moumen Hallak <moumen95@gmail.com>
2021-04-23 13:05:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const array = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'surprise';
+let newArray=[];
+
+
+function writeCards(array, event) {
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = "Thank you, " + array[i] + ", for the wonderful " + event + " gift!";
+  }
+ 
+  return newArray;
+}
+ 
+writeCards(array, event);
+
+
+function countDown(){
+  let countDown = 10;
+  while (countDown > -1) {
+    console.log(countDown--)
+  }
+}
