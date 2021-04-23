82d274eb0c1e60573d4a86c52fb54a96f0262874
Moumen Hallak <moumen95@gmail.com>
2021-04-23 12:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+const array = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'surprise';
+let newArray=[];
+
+
+function writeCards(array, event) {
+  for (let i = 0; i < array.length; i++) {
+    newArray[i] = "Thank you, " + array[i] + ", for the wonderful " + event + " gift!";
+  }
+ 
+  return newArray;
+}
+ 
+writeCards(array, event);
